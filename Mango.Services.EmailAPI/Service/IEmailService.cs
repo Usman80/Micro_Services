4f5208ffd2a04422c82326a7d08e9f2179402410
ManiKhan <usman.bytepeach@gmail.com>
2023-12-17 16:19:49 +0500

    Azure Service Bus Queue Integrated

---
+++ Mango.Services.EmailAPI/Service/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Services.EmailAPI.Models.Dto;
+
+namespace Mango.Services.EmailAPI.Service
+{
+    public interface IEmailService
+    {
+        Task EmailCartAndLog(CartDto cartDto);
+    }
+}
